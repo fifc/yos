fdcf8dba0b3c96ff45a5242e41adcc0065ac57f8
root <root@debian>
2016-10-23 13:36:29 +0800

    initial kernel boot up

---
+++ build.sh
@@ -0,0 +1,8 @@
+cd kernel && make || exit 1
+cd -
+
+rm -rf iso neos.iso
+mkdir -p iso/boot/grub
+cp kernel/neos.bin iso/boot/neos.bin || exit 1
+cp kernel/grub.cfg iso/boot/grub/grub.cfg || exit 2
+grub-mkrescue -o neos.iso iso
