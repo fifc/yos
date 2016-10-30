5aaf624fdffdf334a7e914a1c00fe7d8d2b7ba31
root <root@debian>
2016-10-30 13:55:20 +0800

    cleanup

--- boot/build.bat
+++ boot/build.bat
@@ -2,8 +2,8 @@
 
 REM This script assumes that 'nasm' is in your path
 
-echo | set /p x=Building bmfs_mbr...
-call nasm src\bootsectors\bmfs_mbr.asm -o bmfs_mbr.sys && (echo Success) || (echo Error!)
+echo | set /p x=Building nefs_mbr...
+call nasm src\bootsectors\nefs_mbr.asm -o nefs_mbr.sys && (echo Success) || (echo Error!)
 
 echo | set /p x=Building pxestart...
 call nasm src\bootsectors\pxestart.asm -o pxestart.sys && (echo Success) || (echo Error!)
--- boot/build.sh
+++ boot/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nasm src/bootsectors/bmfs_mbr.asm -o bmfs_mbr.sys
+nasm src/bootsectors/nefs_mbr.asm -o nefs_mbr.sys
 nasm src/bootsectors/pxestart.asm -o pxestart.sys
 cd src
 	nasm boot.asm -o ../boot.sys
